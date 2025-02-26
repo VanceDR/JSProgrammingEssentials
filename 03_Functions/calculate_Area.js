b5d2dc25976e9e79d2fd03df666f4b8738859331
VanceDR <vionet.20@gmail.com>
2025-02-26 13:27:44 +0800

    added functions hands on lab and practice task

---
+++ 03_Functions/calculate_Area.js
@@ -0,0 +1,20 @@
+let length;
+let width;
+
+function calculateArea() {
+    length = parseFloat(document.getElementById('length').value);
+    width = parseFloat(document.getElementById('width').value);
+    let area = length * width;
+    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+
+}
+
+function groceryTracker() {
+    let amount1 = parseFloat(document.getElementById('grocery1').value);
+    let amount2 = parseFloat(document.getElementById('grocery2').value);
+    let amount3 = parseFloat(document.getElementById('grocery3').value);
+
+    let total = amount1 + amount2 + amount3;
+    document.getElementById('groceryTotal').innerText = `The total amount is: $${total}`;
+
+}
