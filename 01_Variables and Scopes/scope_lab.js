03e2f5f99ac7c089a0d7f0b0aa8039273c21568a
VanceDR <vionet.20@gmail.com>
2025-02-26 12:56:56 +0800

    added practice task attempt

--- 01_Variables and Scopes/scope_lab.js
+++ 01_Variables and Scopes/scope_lab.js
@@ -3,9 +3,9 @@ var globalVar = "I'm a global variable";
 let globalLet = "I'm also global, but scoped with let";
 const globalConst = "I'm a global constant";
 
-console.log(globalVar); // Output: "I'm a global variable"
-console.log(globalLet); // Output: "I'm also global, but scoped with let"
-console.log(globalConst); // Output: "I'm a global constant"
+// console.log(globalVar); // Output: "I'm a global variable"
+// console.log(globalLet); // Output: "I'm also global, but scoped with let"
+// console.log(globalConst); // Output: "I'm a global constant"
 
 {
 // Block scope
@@ -25,6 +25,21 @@ function show(){
     const functionConst = "I'm a block-scoped const";
 }
 show();
-console.log(functionVar); // Throws ReferenceError
-console.log(functionLet); // Throws ReferenceError
-console.log(functionConst); // Throws ReferenceError
+// console.log(functionVar); // Throws ReferenceError
+// console.log(functionLet); // Throws ReferenceError
+// console.log(functionConst); // Throws ReferenceError
+
+
+{
+    var blockVar2 = "I'm a block-scoped var";
+    let blockLet2 = "I'm a block-scoped let";
+    const blockConst2 = "I'm a block-scoped const";
+}
+
+blockVar2 = "I'm a block-scoped var - modified";
+blockLet2 = "I'm a block-scoped let - modified";
+blockConst2 = "I'm a block-scoped const - modified";
+
+console.log(blockVar2); // Output: "I'm a block-scoped var - modified"
+console.log(blockLet2); // Output: "I'm a block-scoped let - modified"
+console.log(blockConst2); // Output: "I'm a block-scoped const - modified"
