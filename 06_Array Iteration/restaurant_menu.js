1faf56de4ba4faa7b9e295c0a15e62bced987f9d
VanceDR <vionet.20@gmail.com>
2025-02-26 14:01:38 +0800

    added prices to each element in array

--- 06_Array Iteration/restaurant_menu.js
+++ 06_Array Iteration/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $5.99', 'Eggs Benedict - $7.99', 'Oatmeal - $3.99', 'Frittata - $6.99'];
+const mainCourseMenu = ['Steak - $14.99', 'Pasta - $12.99', 'Burger - $10.99', 'Salmon - $13.99'];
+const dessertMenu = ['Cake - $4.99', 'Ice Cream - $3.99', 'Pudding - $2.99', 'Fruit Salad - $3.49'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
