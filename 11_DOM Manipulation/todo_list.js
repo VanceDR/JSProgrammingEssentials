9343c0a06e694ac58d6d123eb9b79856f424fb2c
VanceDR <vionet.20@gmail.com>
2025-02-26 15:18:04 +0800

    added practice task to clear all tasks

--- 11_DOM Manipulation/todo_list.js
+++ 11_DOM Manipulation/todo_list.js
@@ -3,11 +3,13 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+const clearAllBtn = document.getElementById("clearAllBtn");
 
 let tasks = [];
 
 addTaskBtn.addEventListener("click", addTask);
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+clearAllBtn.addEventListener("click", clearAllTasks);
 
 displayTasks();
 
@@ -38,5 +40,10 @@ function toggleTask(index) {
 
 function clearCompletedTasks() {
     tasks = tasks.filter(task => !task.completed);
+    displayTasks();
+}
+
+function clearAllTasks() {
+    tasks = [];
     displayTasks();
 }
